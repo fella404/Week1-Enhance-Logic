334511d0f6039e577aa83780be58ae5e9c0a0861
fella404 <tomhagen849@gmail.com>
2025-04-15 10:06:32 +0700

    logic-nolep(searchAlgo)-week1-phase1

---
+++ logic_nolep/quiz/2-searchAlgo.js
@@ -0,0 +1,38 @@
+const searchRotatedArray = (nums, target) => {
+  //code
+  let left = 0,
+    right = nums.length;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+
+    if (nums[mid] > nums[left]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
+
+// Test Case
+console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 0)); // Output: 4
+console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 3)); // Output: -1
+console.log(searchRotatedArray([1], 0)); // Output: -1
+console.log(searchRotatedArray([6, 7, 0, 1, 2, 4, 5], 7)); // Output: 1
+console.log(searchRotatedArray([4, 5, 6, 7, 8, 1, 2, 3], 6)); // Output: 2
+console.log(searchRotatedArray([3, 4, 5, 6, 7, 8, 1, 2], 8)); // Output: 5
+console.log(searchRotatedArray([5, 6, 7, 8, 1, 2, 3, 4], 1)); // Output: 4
+console.log(searchRotatedArray([2, 3, 4, 5, 6, 7, 8, 1], 9)); // Output: -1
+console.log(searchRotatedArray([3, 1], 1)); // Output: 1
+console.log(searchRotatedArray([5, 1, 3], 5)); // Output: 0
